d3007232b86c18179fe03d8385f400c38cc38f9a
callanhunter <callan57hunter@gmail.com>
2022-04-18 21:29:59 -0700

    add links to projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // making button work
 
-var cmButton = document.querySelector("#signup");
-var cmModalBackground = document.querySelector(".modal-background");
-var cmModal = document.querySelector(".modal")
+var cmButton = document.querySelector("#cm-button");
+var cmModalBackground = document.querySelector("#cm-background");
+var cmModal = document.querySelector("#celebrity-matchmaker")
 
 cmButton.addEventListener("click", () => {
     cmModal.classList.add("is-active");
@@ -10,4 +10,30 @@ cmButton.addEventListener("click", () => {
 
 cmModalBackground.addEventListener("click", () => {
     cmModal.classList.remove("is-active")
+})
+
+
+
+var wdButton = document.querySelector("#wd-button");
+var wdModalBackground = document.querySelector("#wd-background");
+var wdModal = document.querySelector("#weather-dashboard")
+
+wdButton.addEventListener("click", () => {
+    cmModal.classList.add("is-active");
+})
+
+cmModalBackground.addEventListener("click", () => {
+    cmModal.classList.remove("is-active")
+})
+
+var pgButton = document.querySelector("#pg-button");
+var pgModalBackground = document.querySelector("#pg-background");
+var pgModal = document.querySelector("password-generator")
+
+pgButton.addEventListener("click", () => {
+    pgModal.classList.add("is-active");
+})
+
+pgModalBackground.addEventListener("click", () => {
+    pgModal.classList.remove("is-active")
 })
